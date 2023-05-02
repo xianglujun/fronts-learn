2a478982dbece7393e3ada404af1b990d2f921aa
xianglujun <xianglj1991@163.com>
2023-05-02 17:29:00 +0800

    vue知识学习

---
+++ vue/lib/random_str.js
@@ -0,0 +1,9 @@
+function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
+function random() {
+    return randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+}
