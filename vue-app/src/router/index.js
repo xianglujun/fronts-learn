52aa51f84d8c61be1fdda9f541c4c4af12ce6083
xianglujun <xianglj1991@163.com>
2023-05-13 22:08:49 +0800

    路由守卫学习

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -23,6 +23,11 @@ const routes = [{
   component: () => import('@/views/films/FilmDetail.vue')
 },
 {
+  path: '/login',
+  component: () => import('@/views/LoginView.vue'),
+  props: route => ({ from: route.query.from })
+},
+{
   path: '/camera',
   name: 'camera',
   component: () => import('@/views/CameraView.vue')
@@ -30,20 +35,63 @@ const routes = [{
 {
   path: '/center',
   name: 'center',
-  component: () => import('@/views/CenterView.vue')
+  component: () => import('@/views/CenterView.vue'),
+  meta: {
+    needToken: true
+  },
+  beforeEnter: (to, from, next) => {
+
+    // 判断是否包含了token
+    if (localStorage.getItem('token')) {
+      next(true)
+    } else {
+      // 拼接路径
+      // next(`/login?from=${to.fullPath}`)
+      next({
+        path: '/login',
+        query: {
+          from: to.fullPath
+        }
+      })
+    }
+  }
 },
 {
   // 重定向, *表示当没有匹配到的时候，最后一个匹配规则
   path: '*',
   redirect: '/films'
 }
-
 ]
 
+/* vue支持两种路径模式, history: 则没有#号; hash为有#号，也是默认模式 */
 const router = new VueRouter({
-  // mode: 'history',
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+// 添加全局拦截(守卫)
+router.beforeEach((to, from, next) => {
+  console.log('to', to, from)
+  // 判断目标路由是否需要登录
+  let meta = to.meta;
+  if (meta && meta.needToken) {
+    // 判断是否包含了token
+    if (localStorage.getItem('token')) {
+      next(true)
+    } else {
+      // 拼接路径
+      // next(`/login?from=${to.fullPath}`)
+      next({
+        path: '/login',
+        query: {
+          from: to.fullPath
+        }
+      })
+    }
+  } else {
+    return next(true);
+  }
 })
 
 export default router
---
+++ vue-app/src/views/LoginView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>登陆界面</p>
+    <button @click="handleLogin">登录</button>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      from: {
+        type: String,
+        default: ''
+      }
+    },
+    methods: {
+      handleLogin () {
+        localStorage.setItem('token', '这是登录token信息')
+        console.log('登陆完成, 跳转路径', this.from)
+        //返回上一个界面
+        this.$router.replace(this.from)
+      }
+    }
+  }
+</script>
